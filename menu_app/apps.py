ebefab80d2a10b48365bb1f5a1e76d70087d3504
vldmirr <tsutsura18@gmail.com>
2025-11-24 19:57:27 +0300

    убрал verbose_name, перименовал класс

--- menu_app/apps.py
+++ menu_app/apps.py
@@ -1,7 +1,6 @@
 from django.apps import AppConfig
 
 
-class MenuAppConfig(AppConfig):
+class MenuConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'menu_app'
-    verbose_name = 'Menu Application'
